0e8202a250e5ed4c69082a930c023758ead61675
JoszkinKevin <54977279+JoszkinKevin@users.noreply.github.com>
2023-11-23 15:42:17 +0100

    Update macska.py

--- macska.py
+++ macska.py
@@ -1,7 +1,45 @@
 import streamlit as st
 
-st.write("# Hello World!")
+def osszeadas(szam1, szam2):
+  return szam1 + szam2
 
-st.text_input("nevet", "")
+def kivonas(szam1, szam2):
+  return szam1 - szam2
 
-st.write("Halo")
+def szorzas(szam1, szam2):
+  return szam1 * szam2
+
+def osztas(szam1, szam2):
+  if szam2 != 0:
+    return szam1 / szam2
+  else:
+    return "Nullával nem osztható"
+
+def szazalek(szam, szazalek_ertek):
+  return szam * (szazalek_ertek / 100)
+
+def main():
+  st.title("Számológép")
+
+  szam1 = st.number_input("Első szám:")
+
+  muvelet = st.selectbox("Válassz műveletet:",("összeadás","kivonás","szorzás","osztás","százalékszámítás"))
+
+  szam2 = st.number_input("Második szám:")
+
+  result = 0
+  if muvelet == "összeadás":
+    result = osszeadas(szam1, szam2)
+  elif muvelet == "kivonás":
+    result = kivonas(szam1, szam2)
+  elif muvelet == "szorzás":
+    result = szorzas(szam1, szam2)
+  elif muvelet == "osztás":
+    result = osztas(szam1, szam2)
+  elif muvelet == "százalékszámítás":
+    result = szazalek(szam1, szam2)
+
+  st.write(f"Eredmény: {result}")
+
+if __name__ == '__main__':
+  main()
